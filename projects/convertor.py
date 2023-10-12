ea99cce8737dcf9e6f9f57d34fb3a07dea99526a
Chad ONeal <101817112+chad-oneal@users.noreply.github.com>
2023-10-12 14:55:38 -0400

    GUI coding exercise for day 16

--- projects/convertor.py
+++ projects/convertor.py
@@ -9,8 +9,8 @@ input2 = sg.Input()
 convert_button = sg.Button('Convert')
 
 window = sg.Window('Convertor', layout=[[label1, input1],
-                                              [label2, input2],
-                                              [convert_button]])
+                                        [label2, input2],
+                                        [convert_button]])
 
 window.read()
 window.close()
