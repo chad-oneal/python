aacfb7995c21ec09df2038831745ebe6707bf874
Chad ONeal <101817112+chad-oneal@users.noreply.github.com>
2023-10-12 16:35:51 -0400

    Added while loop for add function

--- gui.py
+++ gui.py
@@ -1,13 +1,28 @@
 import functions
-import PySimpleGUI as simple
+import PySimpleGUI as sg
 
-label = simple.Text('Enter a To-Do')
-input_box = simple.InputText(tooltip='Enter todo')
-add_button = simple.Button('Add')
+sg.theme('DefaultNoMoreNagging')
+label = sg.Text('Enter a To-Do', font=('Karma', 20))
+input_box = sg.InputText(tooltip='Enter todo', key='todo')
+add_button = sg.Button('Add', font='Karma, 14')
+
+window = sg.Window('To-Do App', layout=[[label], [input_box, add_button]],
+                   font=('Karma', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
 
 
-window = simple.Window('To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+
 window.close()
-
-
