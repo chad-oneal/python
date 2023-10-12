e8c3a01e81f82178a06703b337476b5b9e52002e
Chad ONeal <101817112+chad-oneal@users.noreply.github.com>
2023-10-12 19:06:33 -0400

    Updated GUI

--- gui.py
+++ gui.py
@@ -6,13 +6,15 @@ label = sg.Text('Enter a To-Do', font=('Karma', 20))
 input_box = sg.InputText(tooltip='Enter todo', key='todo')
 add_button = sg.Button('Add', font='Karma, 14')
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                     enable_events=True, size=[45, 10])
+                      enable_events=True, size=(45, 10))
 
 edit_button = sg.Button('Edit', font='Karma, 15')
 
-window = sg.Window('To-Do App', layout=[[label], [input_box, add_button],
-                                        [list_box], [edit_button]],
-                                        font=('Karma', 20))
+layout = [[label], [input_box, add_button], [list_box], [edit_button]]
+
+window = sg.Window('To-Do App',
+                   layout=layout,
+                   font=('Karma', 20))
 
 while True:
     event, values = window.read()
