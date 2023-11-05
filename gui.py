01db95e1ffeb1d74b2cb8232d6b7454f34c7d0d1
chad-oneal <coneal@regscale.com>
2023-11-05 11:11:00 -0500

    Updated GUI - added exit and complete buttons

--- gui.py
+++ gui.py
@@ -9,8 +9,11 @@ list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 
 edit_button = sg.Button('Edit', font='Karma, 15')
-
-layout = [[label], [input_box, add_button], [list_box], [edit_button]]
+complete_button =sg.Button('Complete', font='Karma, 15')
+exit_button = sg.Button('Exit', font='Karma, 15', pad=((525,0),0))
+layout = [[label],
+          [input_box, add_button],
+          [list_box], [edit_button, complete_button, exit_button]]
 
 window = sg.Window('To-Do App',
                    layout=layout,
@@ -29,6 +32,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case 'Edit':
             todo_to_edit = values['todos'][0]
@@ -39,6 +43,18 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case 'Exit':
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
